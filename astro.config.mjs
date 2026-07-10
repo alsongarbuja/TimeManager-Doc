f05d38a73132ab850b7977262084f6d70dae1cd8
alsongarbuja <magar33alson@gmail.com>
2026-07-10 17:39:56 -0500

    feat: add workflow to deploy to GP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alsongarbuja.github.io",
+  base: "/TimeManager-Doc/",
   integrations: [
     starlight({
       title: "Time manager Doc",
